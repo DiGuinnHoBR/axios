1b23cf2e92bb32f76a09dc82f1e498036cd787ff
DiGuinnHoBR <130993386+DiGuinnHoBR@users.noreply.github.com>
2023-04-29 16:40:35 -0300

    Initial commit

---
+++ src/axios.js
@@ -0,0 +1,24 @@
+axios("estados.json").then((resposta) =>
+  carregaElementosNaPagina(resposta.data)
+);
+
+function carregaElementosNaPagina(json) {
+  const table = document.createElement("table");
+
+  for (let estados of json) {
+    const tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.innerHTML = estados.nome;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement("td");
+    td1.innerHTML = estados.sigla;
+    tr.appendChild(td2);
+
+    table.appendChild(tr);
+  }
+
+  const resultado = document.querySelector(".resultado");
+  resultado.appendChild(table);
+}
